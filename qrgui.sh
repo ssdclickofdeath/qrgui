7c00f2085bb75c36373df8ad9d81800b2935bf14
ssdclickofdeath <gramex@openmailbox.org>
2014-05-19 15:16:22 -0700

    Fix to exit properly when cancel button pressed, verbose terminal output

--- qrgui.sh
+++ qrgui.sh
@@ -15,18 +15,39 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-TMP=/tmp/stdout
 ICON=qricon.png
 
-#Makes text box for QR code text, and saves it to the QRSTRING variable.
+#Checks if qrencode is installed.
+qrencode -o /dev/null "Test"
+
+if  [ $? = 127 ]; then
+    echo -e "Qrencode (The QR code generator) is not installed.";
+    zenity --info \
+           --window-icon=$ICON \
+           --title="QR Encoder" \
+           --width=400 \
+           --text="The QR code generator (qrencode) is not installed. It must be installed for qrgui to work.
+
+Try installing it with your preferred package manager."
+    exit 1;
+fi
+
+echo "qrgui 0.7"
+echo "Copyright (C) 2014 ssdclickofdeath"
+echo "This is free software; see README for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
+
 QRSTRING=$(zenity --entry \
                   --title="QR Encoder" \
                   --window-icon=$ICON \
                   --text="Enter text to encode into a QR code." \
-                  --width=300) #; echo $? > $TMP)
+                  --width=300)
 
-#Checks if window was closed by looking for exit code 1, if so, the program is closed
-#[ `cat $TMP` = 1 ] && echo "Exiting qrgui..."; rm $TMP; exit 0;
+#Checks if window was closed/canceled by looking
+#for exit code 1, if so, the program is closed.
+if  [ $? = 1 ]; then
+    echo -e "\nExiting qrgui..."; exit 1;
+fi
 
 # Makes an empty variable to compare to QRSTRING to check for empty text entry.
 BLANK=""
@@ -34,30 +55,37 @@ BLANK=""
 #Compares QRSTRING to BLANK to see if text entry field is blank.
 #If the field is blank, make a dialog box showing the error.
 if [ $QRSTRING = $BLANK ]; then
-       zenity --error \
-              --width=270 \
-              --title="QR Encoder" \
-              --window-icon=$ICON \
+    zenity --error \
+           --width=270 \
+           --title="QR Encoder" \
+           --window-icon=$ICON \
      --text="The text entry field cannot be blank. QR Encoder will now close." \
-              --timeout=4
-              exit 1;
+           --timeout=4
+           echo -e "\nExiting qrgui..."
+           exit 1;
 fi
-
-# Please enter some text inside the field. This program will now close.
 
 #Sets the variable FILENAME to the name and path to save the QR code.
 FILENAME=$(zenity --file-selection \
               --window-icon=$ICON \
               --title="Save QR Code - QR Encoder" \
               --save \
-              --confirm-overwrite) #; echo $? > $TMP)
+              --confirm-overwrite)
 
-#Checks if window was closed by looking for exit code 1, if so, the program is closed
-#[ `cat $TMP` = 1 ] && echo "Exiting qrgui..."; rm $TMP; exit 0;
+#Checks if window was closed/canceled by looking
+#for exit code 1, if so, the program is closed.
+if  [ $? = 1 ]; then
+    echo -e "\nExiting qrgui..."; exit 1;
+fi
 
 #Generates the QR code.
 qrencode -o "$FILENAME" "$QRSTRING"
 
-#Exits the shell script.
-#rm $TMP
+#Checks if QR code was generated successfully.
+if  [ $? = 0 ]; then
+    echo -e "\nQR code generated successfully"; exit 0;
+                else
+    echo -e "\nAn error occured."; exit 1;
+fi
+
 exit 0;
